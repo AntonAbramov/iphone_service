1adfac3ed4998e6cd9ed9d9c632d3244c6216048
anton <AntonAbramov1990@gmail.com>
2015-02-02 00:45:52 +0200

    views for priceList Iphone

---
+++ js/directives/backup_iphone_view.js
@@ -0,0 +1,289 @@
+angular.module('iApp').directive('pricesIphone4gs', function () {
+  return {
+    restrict: 'E',
+    templateUrl: '/js/views/prices-iphone.html',
+    controller: function ($scope){
+      $scope.pricesTable = [
+        {
+          'tableTitle': 'Основное',
+          'rows' : [
+            {
+              'title': '',
+              'repear_time': 'от 777 мин.',
+              'cost': '99999 грн.',
+              'description': ''
+            },
+            {
+              'title': '',
+              'repear_time': 'от 777 мин.',
+              'cost': '99999 грн.',
+              'description': ''
+            },
+            {
+              'title': '',
+              'repear_time': 'от 777 мин.',
+              'cost': '99999 грн.',
+              'description': ''
+            },
+            {
+              'title': '',
+              'repear_time': 'от 777 мин.',
+              'cost': '99999 грн.',
+              'description': ''
+            }
+
+          ]
+        },
+        {
+          'tableTitle': 'Корпус',
+          'rows' : [
+            {
+              'title': '',
+              'repear_time': 'от 777 мин.',
+              'cost': '99999 грн.',
+              'description': ''
+            },
+            {
+              'title': '',
+              'repear_time': 'от 777 мин.',
+              'cost': '99999 грн.',
+              'description': ''
+            },
+            {
+              'title': '',
+              'repear_time': 'от 777 мин.',
+              'cost': '99999 грн.',
+              'description': ''
+            },
+            {
+              'title': '',
+              'repear_time': 'от 777 мин.',
+              'cost': '99999 грн.',
+              'description': ''
+            }
+          ]
+        },
+        {
+          'tableTitle': 'Кнопки',
+          'rows' : [
+            {
+              'title': '',
+              'repear_time': 'от 777 мин.',
+              'cost': '99999 грн.',
+              'description': ''
+            },
+            {
+              'title': '',
+              'repear_time': 'от 777 мин.',
+              'cost': '99999 грн.',
+              'description': ''
+            },
+            {
+              'title': '',
+              'repear_time': 'от 777 мин.',
+              'cost': '99999 грн.',
+              'description': ''
+            },
+            {
+              'title': '',
+              'repear_time': 'от 777 мин.',
+              'cost': '99999 грн.',
+              'description': ''
+            }
+
+            
+          ]
+        },
+        {
+          'tableTitle': 'Комплектующие',
+          'rows' : [
+            {
+              'title': '',
+              'repear_time': 'от 777 мин.',
+              'cost': '99999 грн.',
+              'description': ''
+            },
+            {
+              'title': '',
+              'repear_time': 'от 777 мин.',
+              'cost': '99999 грн.',
+              'description': ''
+            },
+            {
+              'title': '',
+              'repear_time': 'от 777 мин.',
+              'cost': '99999 грн.',
+              'description': ''
+            },
+            {
+              'title': '',
+              'repear_time': 'от 777 мин.',
+              'cost': '99999 грн.',
+              'description': ''
+            },
+            {
+              'title': '',
+              'repear_time': 'от 777 мин.',
+              'cost': '99999 грн.',
+              'description': ''
+            },
+            {
+              'title': '',
+              'repear_time': 'от 777 мин.',
+              'cost': '99999 грн.',
+              'description': ''
+            },
+            {
+              'title': '',
+              'repear_time': 'от 777 мин.',
+              'cost': '99999 грн.',
+              'description': ''
+            },
+            {
+              'title': '',
+              'repear_time': 'от 777 мин.',
+              'cost': '99999 грн.',
+              'description': ''
+            },
+            {
+              'title': '',
+              'repear_time': 'от 777 мин.',
+              'cost': '99999 грн.',
+              'description': ''
+            },
+            {
+              'title': '',
+              'repear_time': 'от 777 мин.',
+              'cost': '99999 грн.',
+              'description': ''
+            },
+            {
+              'title': '',
+              'repear_time': 'от 777 мин.',
+              'cost': '99999 грн.',
+              'description': ''
+            },
+            {
+              'title': '',
+              'repear_time': 'от 777 мин.',
+              'cost': '99999 грн.',
+              'description': ''
+            },
+            {
+              'title': '',
+              'repear_time': 'от 777 мин.',
+              'cost': '99999 грн.',
+              'description': ''
+            },
+            {
+              'title': '',
+              'repear_time': 'от 777 мин.',
+              'cost': '99999 грн.',
+              'description': ''
+            },
+            {
+              'title': '',
+              'repear_time': 'от 777 мин.',
+              'cost': '99999 грн.',
+              'description': ''
+            },
+            {
+              'title': '',
+              'repear_time': 'от 777 мин.',
+              'cost': '99999 грн.',
+              'description': ''
+            },
+            {
+              'title': '',
+              'repear_time': 'от 777 мин.',
+              'cost': '99999 грн.',
+              'description': ''
+            }
+          ]
+        },
+        {
+          'tableTitle': 'Шлейфы',
+          'rows' : [
+            {
+              'title': '',
+              'repear_time': 'от 777 мин.',
+              'cost': '99999 грн.',
+              'description': ''
+            },
+            {
+              'title': '',
+              'repear_time': 'от 777 мин.',
+              'cost': '99999 грн.',
+              'description': ''
+            },
+            {
+              'title': '',
+              'repear_time': 'от 777 мин.',
+              'cost': '99999 грн.',
+              'description': ''
+            },
+            {
+              'title': '',
+              'repear_time': 'от 777 мин.',
+              'cost': '99999 грн.',
+              'description': ''
+            },
+            {
+              'title': '',
+              'repear_time': 'от 777 мин.',
+              'cost': '99999 грн.',
+              'description': ''
+            },
+            {
+              'title': '',
+              'repear_time': 'от 777 мин.',
+              'cost': '99999 грн.',
+              'description': ''
+            }
+          ]
+        },
+        {
+          'tableTitle': 'Другое',
+          'rows' : [
+            {
+              'title': 'Замена мат. платы 16 Gb',
+              'repear_time': 'от 777 мин.',
+              'cost': '99999 грн.',
+              'description': 'Заменять материнскую плату необходимо при полной неработоспособности старой платы. Это самая дорогая деталь в телефоне.'
+            },
+            {
+              'title': 'Замена мат. платы 32 Gb',
+              'repear_time': 'от 777 мин.',
+              'cost': '99999 грн.',
+              'description': 'Заменять материнскую плату необходимо при полной неработоспособности старой платы. Это самая дорогая деталь в телефоне.'
+            },
+            {
+              'title': 'Замена мат. платы 64 Gb',
+              'repear_time': 'от 777 мин.',
+              'cost': '99999 грн.',
+              'description': 'Заменять материнскую плату необходимо при полной неработоспособности старой платы. Это самая дорогая деталь в телефоне.'
+            },
+            {
+              'title': 'Наклейка пленки',
+              'repear_time': 'от 777 мин.',
+              'cost': '99999 грн.',
+              'description': 'Пленка поможет избежать Вам царапин и повреждений на переднем стекле. Стоимость указана за наклейку пленки с одной стороны и с учетом самой пленки.'
+            },
+            {
+              'title': 'Доставка из сервиса',
+              'repear_time': 'от 777 мин.',
+              'cost': '99999 грн.',
+              'description': 'Если у Вас нет времени забрать Ваш iPhone 5, наш курьер всегда с радостью Вам его доставит.'
+            },
+            {
+              'title': 'Доставка в сервис',
+              'repear_time': 'от 777 мин.',
+              'cost': '99999 грн.',
+              'description': 'Курьер сможет приехать в удобное для Вас время и совершенно бесплатно.'
+            }
+          ]
+        }
+      ];
+    }
+  }
+});
